f09c49eb53ab49c88a236e330e79feff10704633
Krypf <krypf2@gmail.com>
2025-03-21 04:19:22 +0900

    create a folder

---
+++ arXiv/new.sh
@@ -0,0 +1,7 @@
+open https://arxiv.org/list/hep-th/new
+open https://arxiv.org/list/math-ph/new
+open https://arxiv.org/list/gr-qc/new
+open https://arxiv.org/list/quant-ph/new
+open https://arxiv.org/list/hep-ph/new
+open https://arxiv.org/list/astro-ph/new
+open https://arxiv.org/list/cond-mat/new
---
+++ arXiv/recent.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/zsh
+for i in hep-th gr-qc math-ph quant-ph hep-ph astro-ph  cond-mat
+do
+   open https://arxiv.org/list/$i/recent
+done
