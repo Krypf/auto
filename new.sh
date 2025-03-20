5caa077196cf8745e586371b4caeb2d9c142098d
Krypf <krypf2@gmail.com>
2025-03-21 04:20:21 +0900

    delete mode

--- new.sh
+++
@@ -1,7 +0,0 @@
-open https://arxiv.org/list/hep-th/new
-open https://arxiv.org/list/math-ph/new
-open https://arxiv.org/list/gr-qc/new
-open https://arxiv.org/list/quant-ph/new
-open https://arxiv.org/list/hep-ph/new
-open https://arxiv.org/list/astro-ph/new
-open https://arxiv.org/list/cond-mat/new
