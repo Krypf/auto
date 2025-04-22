f6713dbbe0b6d379f59a984d0aae2e066d1aa563
Krypf <krypf2@gmail.com>
2025-04-22 21:41:17 +0900

    make main

--- open/WeatherNews_driver.py
+++ open/WeatherNews_driver.py
@@ -1,41 +1,38 @@
 # %% open "today and tomorrow" in Firefox
-# import time
-# t = time.time()
 from selenium import webdriver
 import os
 from dotenv import load_dotenv
-load_dotenv(dotenv_path="open/weather.env")
-location = os.getenv("WEATHER_LOCATION")
-weather_url = os.getenv(f"WEATHER_URL_{location}")
-
-profile = webdriver.FirefoxProfile()
-extensions_dirpath = os.path.expanduser(os.getenv("EXTENSIONS_PATH"))
-extensions = [
-    os.path.join(extensions_dirpath, os.getenv("EXTENSION_1")),
-    os.path.join(extensions_dirpath, os.getenv("EXTENSION_2")),
-]
-
-driver = webdriver.Firefox(firefox_profile=profile,executable_path='/usr/local/bin/geckodriver')
-
-for extension in extensions:
-    # profile.add_extension(extension)
-    driver.install_addon(extension, temporary=True)
-# profile.set_preference("extensions.adblockplus.currentVersion", "2.4")
-
-driver.get(weather_url)
-driver.switch_to.window(driver.window_handles[0])
-element = driver.find_elements_by_class_name("switchTab__item")
-# element[1].click() # 今日明日
-# element[2].click() # 週間
-j = 2
-element[j].click()
-# print(time.time() - t,'seconds'); t = time.time()
 
 def main():
+    load_dotenv(dotenv_path="open/weather.env")
+    location = os.getenv("WEATHER_LOCATION")
+    weather_url = os.getenv(f"WEATHER_URL_{location}")
+
+    extensions_dirpath = os.path.expanduser(os.getenv("EXTENSIONS_PATH"))
+    extensions = [
+        os.path.join(extensions_dirpath, os.getenv("EXTENSION_1")),
+        os.path.join(extensions_dirpath, os.getenv("EXTENSION_2")),
+    ]
+
     # Launch Firefox
-    
+    profile = webdriver.FirefoxProfile()
+    driver = webdriver.Firefox(firefox_profile=profile,executable_path='/usr/local/bin/geckodriver')
+
+    for extension in extensions:
+        driver.install_addon(extension, temporary=True)
+
+    driver.get(weather_url)
+    driver.switch_to.window(driver.window_handles[0])
+    element = driver.find_elements_by_class_name("switchTab__item")
+    j = 1 # 今日明日
+    j = 2 # 週間
+    element[j].click()
     return 0
 
 if __name__ == '__main__':
+    # import time
+    # t = time.time()
     main()
+    # print(time.time() - t,'seconds'); t = time.time()
+    
 # %%
