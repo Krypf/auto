e3c9f0764a775d0a95c263dc8cbfb025e2ce1a09
Krypf <krypf2@gmail.com>
2025-05-19 04:48:16 +0900

    add yahoo

--- shop/shopping.sh
+++ shop/shopping.sh
@@ -1,17 +1,28 @@
 #!/bin/zsh
 
-if [[ -z "$1" ]]; then
+cd $HOME/auto
+
+KEYWORDS="$1"
+
+if [[ -z $KEYWORDS ]]; then
     echo "Usage: $0 <search_query>"
     exit 1
 fi
 
-query=$(echo "$1" | jq -sRr @uri)  # encode URL
+source shop/shop.env
+
+query=$(echo $KEYWORDS | jq -sRr @uri)  # encode URL
 urls=(
     "https://www.biccamera.com/bc/main"
     "https://www.yodobashi.com/?word=${query}"
     "https://www.amazon.co.jp/s?k=${query}"
+    "${YAHOO_BASE_URL}${query}"
 )
 
 for url in "${urls[@]}"; do
     open "$url"
 done
+
+echo "Search completed for: $KEYWORDS"
+echo "$KEYWORDS" | pbcopy
+echo "Copied: $KEYWORDS"
