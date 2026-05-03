638dfed9dd48fae4d40004494631125fa1173ef9
“Krypf” <krypf2@gmail.com>
2026-05-04 00:26:47 +0900

    Reduce the elements of the list

--- arXiv/new.sh
+++ arXiv/new.sh
@@ -2,7 +2,9 @@
 # https://chatgpt.com/share/67dc7d32-bd40-800e-b215-db0358aa5e99
 
 # Define the list of categories
-categories=(hep-th gr-qc math-ph quant-ph hep-ph astro-ph cond-mat)
+categories=(hep-th gr-qc math-ph)
+# memo
+#  quant-ph hep-ph astro-ph cond-mat
 
 # Iterate over the categories and open each one in a new tab/window
 for category in "${categories[@]}"; do
